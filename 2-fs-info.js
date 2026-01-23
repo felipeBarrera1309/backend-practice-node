705de98bedd8bb3f48f970b7d9af1e486b56dd65
Yave Pico <picobarrera@gmail.com>
2026-01-22 20:26:58 -0500

    main - fs asincrono

--- 2-fs-info.js
+++ 2-fs-info.js
@@ -1,8 +1,9 @@
-const fs = require('fs')
+import fs from 'fs/promises'
 
-fs.writeFileSync('./newFile.js', '//This is the file content')
-
-
-
-
-
+Promise.all([
+	fs.readFile('./1-os-info.js', 'utf-8'),
+	fs.readFile('./newFile.js', 'utf-8')
+]).then(([contentOne, contentTwo]) => {
+	console.log('Content first file: ', contentOne)
+	console.log('Content second file: ', contentTwo)
+}).catch(error => console.log('Este es el error por el cual no pudo hacer todos los procesos en paralelos, uno o varios fallaron: ', error))
