cdc2d9effa0cca3a1c43c0d5f8e05637c469e3f6
Yave Felipe <felipebarrera.gbp@gmail.com>
2026-05-22 16:06:32 -0500

    changes

--- 5.ls-advanced.js
+++ 5.ls-advanced.js
@@ -1,32 +1,47 @@
 import fs from 'fs/promises'
 import path from 'node:path'
 
-/* Quiero hacer un ls, de tal manera que al ejecutar el archivo y pasar un directorio me liste todos los archivos y carpetas */
-
-async function ls(identify) {
+async function readdir(folder) {
+	let content
 	try {
-		const data = await fs.readdir(identify)
-		return data
+		content = await fs.readdir(folder)
 	} catch (error) {
-		console.log('Error al leer contenido: ', error)
+		console.log('No pudo leer correctamente el contenido: ', error)
+		process.exit(1)
 	}
+
 }
 
-/* Hacer lista de contenido de carpeta con nombre, identificar si es archivo o carpeta, tamaño y fecha */
 
-fs.readdir(process.argv[2] ?? '.')
-	.then(async data => {
-		const contentDir = data.map(async item => {
-			const route = path.join(process.argv[2] ?? '.', item)
-			const stats = await fs.stat(route)
-			const isFile = stats.isFile() ? 'File': 'Dir'
-			const size = stats.size.toString()
-			const data = stats.mtime.toLocaleString()
+// import fs from 'fs/promises'
+// import path from 'node:path'
 
-			return `- ${ isFile } ${ item.padEnd(25) } ${ size.padEnd(10) } ${ data }`
-		})
+// /* Quiero hacer un ls, de tal manera que al ejecutar el archivo y pasar un directorio me liste todos los archivos y carpetas */
 
-		const getData = await Promise.all(contentDir)
-		getData.forEach(item => console.log(item))
-	})
-	.catch(error => console.log('Error: ', error))
+// async function ls(identify) {
+// 	try {
+// 		const data = await fs.readdir(identify)
+// 		return data
+// 	} catch (error) {
+// 		console.log('Error al leer contenido: ', error)
+// 	}
+// }
+
+// /* Hacer lista de contenido de carpeta con nombre, identificar si es archivo o carpeta, tamaño y fecha */
+
+// fs.readdir(process.argv[2] ?? '.')
+// 	.then(async data => {
+// 		const contentDir = data.map(async item => {
+// 			const route = path.join(process.argv[2] ?? '.', item)
+// 			const stats = await fs.stat(route)
+// 			const isFile = stats.isFile() ? 'File': 'Dir'
+// 			const size = stats.size.toString()
+// 			const data = stats.mtime.toLocaleString()
+
+// 			return `- ${ isFile } ${ item.padEnd(25) } ${ size.padEnd(10) } ${ data }`
+// 		})
+
+// 		const getData = await Promise.all(contentDir)
+// 		getData.forEach(item => console.log(item))
+// 	})
+// 	.catch(error => console.log('Error: ', error))
